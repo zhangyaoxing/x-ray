4f329903a2e66a7f53e898a1abf05259c0e5a61f
Yaoxing <yaoxing.zhang@gmail.com>
2025-08-18 20:28:52 +0200

    + visualize build info item.

--- libs/check_items/build_info_item.py
+++ libs/check_items/build_info_item.py
@@ -1,5 +1,5 @@
 from libs.check_items.base_item import BaseItem
-from libs.shared import SEVERITY
+from libs.shared import SEVERITY, discover_nodes, enum_all_nodes, enum_result_items
 from libs.utils import *
 
 class BuildInfoItem(BaseItem):
@@ -11,24 +11,60 @@ class BuildInfoItem(BaseItem):
     def test(self, *args, **kwargs):
         self._logger.info(f"Gathering server build information...")
         client = kwargs.get("client")
-        result = client.admin.command("buildInfo")
-        self._logger.info(f"Testing server build information...")
-        eol_version = self._config.get("eol_version", [4, 4, 0])
-        sample_version = result.get("versionArray", None)
-        if not sample_version:
-            self._logger.warning(yellow("Failed to retrieve server build information."))
-            return False
-        if sample_version[0] < eol_version[0] or \
-           (sample_version[0] == eol_version[0] and sample_version[1] < eol_version[1]):
-            self.append_test_result(
-                "cluster",
-                SEVERITY.HIGH,
-                "Server Version EOL",
-                f"Server version {sample_version} is below EOL version {eol_version}."
-            )
+        parsed_uri = kwargs.get("parsed_uri")
+        nodes = discover_nodes(client, parsed_uri)
+        def func_node(set_name, node, **kwargs):
+            raw_result = client.admin.command("buildInfo")
+            test_result = []
+            eol_version = self._config.get("eol_version", [4, 4, 0])
+            running_version = raw_result.get("versionArray", None)
+            if not running_version:
+                self._logger.warning(yellow("Failed to retrieve server build information."))
+                return False
+            if running_version[0] < eol_version[0] or \
+            (running_version[0] == eol_version[0] and running_version[1] < eol_version[1]):
+                test_result.append({
+                    "host": "cluster",
+                    "severity": SEVERITY.HIGH,
+                    "title": "Server Version EOL",
+                    "description": f"Server version {running_version} is below EOL version {eol_version}. Consider upgrading to the latest version."
+                })
+            self.append_test_results(test_result)
+            
+            return test_result, raw_result
 
-        self.captured_sample = result
+        raw_result = enum_all_nodes(nodes, func_mongos_member=func_node, func_rs_member=func_node, func_shard_member=func_node, func_config_member=func_node)
+
+        self.captured_sample = raw_result
 
     @property
     def review_result(self):
-        captured = self.captured_sample
+        result = self.captured_sample
+        table = {
+            "type": "table",
+            "caption": f"Server Build Information",
+            "columns": [
+                {"name": "Component", "type": "string"},
+                {"name": "Host", "type": "string"},
+                {"name": "Version", "type": "string"},
+                {"name": "OpenSSL", "type": "string"},
+                {"name": "Target Arch", "type": "string"},
+                {"name": "Target OS", "type": "string"}
+            ],
+            "rows": []
+        }
+        def func_node(name, node, **kwargs):
+            result = node["rawResult"]
+            build_env = result.get("buildEnvironment", {})
+            table["rows"].append([name, node["host"], 
+                                  result.get("version", ""),
+                                  result.get("openssl", {}).get("running", ""),
+                                  build_env.get("target_arch", ""),
+                                  build_env.get("target_os", "")])
+        enum_result_items(result, func_mongos_member=func_node, func_rs_member=func_node, func_shard_member=func_node, func_config_member=func_node)
+        
+        return {
+            "name": self.name,
+            "description": self.description,
+            "data": [table]
+        }
