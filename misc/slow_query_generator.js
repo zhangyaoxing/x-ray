24a46cdef7e7913e41aebd4babf58d6edf8d2946
Yaoxing <yaoxing.zhang@gmail.com>
2025-09-24 16:34:30 +0200

    + add script for generating slow queries. + add a log file for testing.

---
+++ misc/slow_query_generator.js
@@ -0,0 +1,36 @@
+db = db.getSiblingDB("Restaurant");
+db.setProfilingLevel(0, 0);
+db.pizzas.drop();
+db.pizzas.insertMany([
+    { _id: 3, type: 'beef', size: 'medium', price: 6 },
+    { _id: 1, type: 'cheese', size: 'medium', price: 8 },
+    { _id: 2, type: 'tofu', size: 'small', price: 4 },
+    { _id: 4, type: 'sausage', size: 'large', price: 10 },
+    { _id: 5, type: 'pineapple', size: 'large', price: 100 }
+])
+db.pizzas.createIndex({ type: 1 });
+
+db.pizzas.find({ type: 'beef' });
+db.pizzas.find({ type: 'beef' }).sort({price: -1});
+db.pizzas.find({ 
+    size: { 
+        $in: ["small", "medium", "large"] 
+    } 
+});
+db.pizzas.find({ 
+    size: { 
+        $in: ["small", "medium", "large"] 
+    } 
+}).sort({ price: -1 });
+db.pizzas.find({ 
+    size: { 
+        $in: ["small", "medium", "large"] 
+    } 
+}).sort({ type: 1 });
+
+db.pizzas.aggregate([
+    { $match: { size: { $in: ["small", "medium", "large"] } } },
+    { $group: { _id: "$size", count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+])
+db.setProfilingLevel(0, 100);
