662ebf6cc9a6b66275f95091a9ad672501d811c6
Yaoxing <yaoxing.zhang@gmail.com>
2025-11-07 22:30:11 +0100

    * fix format setting so the on-save formatter is the same as `make format`

--- tests/log/test_shared.py
+++ tests/log/test_shared.py
@@ -8,10 +8,7 @@ def test_to_json():
     v = Version.parse("1.2.3")
     obj = {"timestamp": dt, "message": "Test log", "version": v}
     json_str = to_json(obj)
-    assert (
-        json_str
-        == '{"timestamp": "2024-06-01T12:30:45", "message": "Test log", "version": "1.2.3"}'
-    )
+    assert json_str == '{"timestamp": "2024-06-01T12:30:45", "message": "Test log", "version": "1.2.3"}'
     json_str_indented = to_json(obj, indent=2)
     expected_indented = """{
   "timestamp": "2024-06-01T12:30:45",
